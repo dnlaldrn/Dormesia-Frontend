61eeae6edb169b5caac07613dce83f189305acf5
Daniel Manjares <danielaldreenm@gmail.com>
2026-04-18 15:23:59 +0800

    Add routerlink

--- src/app/Components/hero-section-component/hero-section-component.ts
+++ src/app/Components/hero-section-component/hero-section-component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-hero-section-component',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './hero-section-component.html',
   styleUrl: './hero-section-component.css',
 })
