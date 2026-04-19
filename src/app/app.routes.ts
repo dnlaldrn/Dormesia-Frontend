6704f12fb41b5fbd0cc901b1af0edb7f76ad6ea9
Daniel Manjares <danielaldreenm@gmail.com>
2026-04-19 23:56:39 +0800

    Add routing for the about page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './pages/home-page/home-page';
+import {AboutPage} from './pages/about-page/about-page'
 import { LoginPage } from './pages/login-page/login-page';
 import { SignUpPage } from './pages/sign-up-page/sign-up-page';
 
 export const routes: Routes = [
   { path: '', component: HomePage },
+  { path: 'about', component: AboutPage },
   { path: 'login', component: LoginPage },
   { path: 'sign-up', component: SignUpPage },
 ];
